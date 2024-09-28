0b9ae4a6141ce199c431b9578b5d2aea9bf87d62
zinum-dev <161540422+zinum-dev@users.noreply.github.com>
2024-09-27 21:28:29 -0300

    add chapter

---
+++ week02/scripts/bom.js
@@ -0,0 +1,34 @@
+const input = document.getElementById("favchap");
+const button = document.querySelector("button");
+const list = document.getElementById("list");
+
+
+
+
+
+
+
+
+
+
+button.addEventListener("click",function(){
+    console.log(input.value)
+    if(input.value.trim() !== ''){
+        const li = document.createElement('li');
+        li.textContent = input.value;
+        const deleteButton = document.createElement("button");
+        deleteButton.textContent = '❌';
+        li.append(deleteButton);
+        list.append(li);
+        input.value = '';
+
+        deleteButton.addEventListener("click",function(){
+            list.removeChild(li);
+            input.focus();
+        })
+    }
+    else{
+        alert("invalid input!");
+    }
+    input.focus();
+})
