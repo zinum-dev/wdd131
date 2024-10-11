523860b648a61c9bd3b7202d87388df3f7fa0a3d
zinum-dev <161540422+zinum-dev@users.noreply.github.com>
2024-10-11 08:27:17 -0300

    create file and start

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -4,10 +4,27 @@ oLastModif = Intl.DateTimeFormat("en-US", {
   dateStyle: "full",
 }).format(oLastModif);
 
-
-document.getElementById(
-  "currentyear"
-).textContent = `${year}`;
+document.getElementById("currentyear").textContent = `${year}`;
 document.getElementById(
   "lastModified"
 ).textContent = `Last Modification: ${oLastModif}`;
+
+"Welcome to the " +
+  courseName +
+  " " +
+  stuFirstName +
+  "!"
+  `Welcome to the ${courseName} ${stuFirstName}!`;
+
+let esomeprazole = {
+  id: "mnb78932",
+  amount: 23145,
+  amountType: "ct",
+  expDate: "10/01/2021",
+};
+esomeprazole.amount;
+
+esomeprazole["amount"];
+
+const countries = ["Canada", "Chili", "Colombia", "Croatia", "Costa Rica"];
+const countriesLong = countries.filter((country) => country.length > 7);
