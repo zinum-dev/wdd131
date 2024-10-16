e03d5cbfa69ba3992d59fc2098a475a8262231b4
zinum-dev <161540422+zinum-dev@users.noreply.github.com>
2024-10-16 08:36:45 -0300

    resolving errors

--- scripts/form.js
+++ scripts/form.js
@@ -12,11 +12,3 @@ document.getElementById(
   "lastModified"
 ).textContent = `Last Modification: ${oLastModif}`;
 
-
-const hamButton = document.querySelector('#menu');
-const navigation = document.querySelector('.navigation');
-
-hamButton.addEventListener('click', () => {
-	navigation.classList.toggle('open');
-	hamButton.classList.toggle('open');
-});
